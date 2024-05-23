388ef69697c3f487c10ab93587a001d4e5c42390
David <david.santiagonino20@gmail.com>
2024-05-22 20:09:58 -0500

    se borra el package-lock y la carpet node_modules

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 console.log("coronamos")
+let express = require("express")
+const servidor = express()
+
+servidor.get("/", (peticiones,respuestas)=>{
+  respuestas.json({
+    saludo: "hola, vamos bien"
+  })  
+} )
+
+servidor.listen(3000)
