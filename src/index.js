6935cc3b214d5b0c2d1036272325627353c6436c
David <david.santiagonino20@gmail.com>
2024-07-25 20:44:43 -0500

    cambio de puerto

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import servidor from "./servidor.js";
 //se importa el archivo que esta llamando al moongose osea el que esta haciendo la conexión entre la BD
 import "./baseDeDatos.js";
 
-servidor.listen(2000, () => {
+servidor.listen(3000, () => {
   console.log("Servidor corriendo en el puerto 3000");
 });
+d;
